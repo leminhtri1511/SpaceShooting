f4e7b6a160a242913edb203948612a47c3404ec8
leminhtri1511 <leminhtri151102@gmail.com>
2025-05-02 16:06:59 +0700

    feat: auto spawn enemies with instantiate()

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO currentWave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    public WaveConfigSO GetCurrentWave()
+    {
+        return currentWave;
+    }
+
+    private void SpawnEnemies()
+    {
+        for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+        {
+            Instantiate(
+            currentWave.GetEnemyPrefab(i),
+            currentWave.GetStartingWayPoint().position,
+            Quaternion.identity,
+            transform
+        );
+        }
+
+    }
+}
--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Wave Config", menuName = "Wave Config", order = 1)]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;    
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
 
     public Transform GetStartingWayPoint()
     {
